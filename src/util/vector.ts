f6db58fe595580f3535f17e299875e63ee0607df
Hugos29 <y.hugo.s29@gmail.com>
2020-10-23 19:39:49 +0200

    Remove HSV, HSL from Color (it wasn't working well), added more methods with vectors

--- src/util/vector.ts
+++ src/util/vector.ts
@@ -1,3 +1,6 @@
+import Matrix from './matrix'
+import { TAU } from '../main'
+
 export type VectorValues = [number, number]
 
 export class Vector {
@@ -7,12 +10,19 @@ export class Vector {
     this.values = [0, 0]
   }
 
+  public copy() {
+    const [x, y] = this.values
+    return Vector.FROM(x, y)
+  }
+
   public add(v: Vector) {
     this.values = this.values.map((val, i) => val + v.values[i]) as VectorValues
+    return this
   }
 
   public substract(v: Vector) {
     this.values = this.values.map((val, i) => val - v.values[i]) as VectorValues
+    return this
   }
 
   public magSq() {
@@ -24,6 +34,29 @@ export class Vector {
     return Math.sqrt(this.magSq())
   }
 
+  public mult(c: number) {
+    this.values = this.values.map((v) => v * c) as VectorValues
+    return this
+  }
+
+  public div(c: number) {
+    this.mult(1 / c)
+    return this
+  }
+
+  public invert() {
+    this.values = this.values.map((v) => 1 / v) as VectorValues
+    return this
+  }
+
+  public applyMatrix(m: Matrix) {
+    const x = this.values[0]
+    const y = this.values[1]
+    this.values[0] = m.values[0] * x + m.values[2] * y + m.values[4]
+    this.values[1] = m.values[1] * x + m.values[3] * y + m.values[5]
+    return this.values
+  }
+
   static FROM(...values: VectorValues) {
     const vec = new Vector()
     vec.values = [...values] as VectorValues
@@ -32,6 +65,10 @@ export class Vector {
 
   static POLAR(θ: number, r: number) {
     return this.FROM(r * Math.cos(θ), r * Math.sin(θ))
+  }
+
+  static RANDOM(θ: number = Math.random() * TAU, r: number = Math.random() * 5) {
+    return this.POLAR(θ, r)
   }
 }
 
